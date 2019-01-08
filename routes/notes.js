ed79eac49e849efb0a23c91725658a47def7f9b2
mischb <mischarberlin@gmail.com>
2019-01-08 16:37:26 -0500

    update get endpoint for api/notes

--- routes/notes.js
+++ routes/notes.js
@@ -6,15 +6,24 @@ const passport = require('passport');
 
 router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 
-router.get('/', (req, res) => {
-    let UserId = req.user.id;
-    User.find()
-    .then(() => {
 
+router.get('/', (req, res) => {
+  let id = req.user.id;
+  User.findOne({_id: id})
+    .then((user) => {
+        const {notes, next} = user;
+        const returnNote = notes.filter(note => {
+            if(note.note === next){
+                return note;
+            }
+        })
+        res.json(returnNote[0])
     })
     .catch(err => {
-        console.log(err);
+      console.log(err);
     });
 });
+
+
 
 module.exports = router;
